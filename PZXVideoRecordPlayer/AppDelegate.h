16d4bb240edd8c79d20f6644e2cc41f0999ee7d7
pzx <496912046@qq.com>
2018-09-11 17:05:51 +0800

    Initial Commit

---
+++ PZXVideoRecordPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PZXVideoRecordPlayer
+//
+//  Created by xrkj on 2018/9/11.
+//  Copyright © 2018年 pengzuxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PZXVideoRecordPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PZXVideoRecordPlayer
+//
+//  Created by xrkj on 2018/9/11.
+//  Copyright © 2018年 pengzuxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
