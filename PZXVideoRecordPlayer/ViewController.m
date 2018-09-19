de278d5d804ebcb6ee4bf7d041d4d95f0ce494dc
pzx <496912046@qq.com>
2018-09-19 11:13:06 +0800

    添加 增添水印方法。

--- PZXVideoRecordPlayer/ViewController.m
+++ PZXVideoRecordPlayer/ViewController.m
@@ -9,8 +9,11 @@
 #import "ViewController.h"
 #import "PZXVideoView.h"
 #import "PZXPlayerViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
 @interface ViewController ()<PZXVideoViewDelegate>
 @property (nonatomic, strong) PZXVideoView *videoView;
+@property (nonatomic, strong) AVAsset *videoAsset;
 
 @end
 
@@ -45,9 +48,179 @@
 - (void)recordFinishWithvideoUrl:(NSURL *)videoUrl
 {
     
+    [self addSYWithUrl:videoUrl];//添加水印方法可以不要
+//    PZXPlayerViewController *playVC = [[PZXPlayerViewController alloc] init];
+//    playVC.videoUrl =  videoUrl;
+//    
+//    [self.navigationController pushViewController:playVC animated:YES];
+  
+}
+
+-(void)addSYWithUrl:(NSURL *)videoUrl{
     
-    PZXPlayerViewController *playVC = [[PZXPlayerViewController alloc] init];
-    playVC.videoUrl =  videoUrl;
-    [self.navigationController pushViewController:playVC animated:YES];
+    self.videoAsset = [AVAsset assetWithURL:videoUrl];
+    //2 创建AVMutableComposition实例. apple developer 里边的解释 【AVMutableComposition is a mutable subclass of AVComposition you use when you want to create a new composition from existing assets. You can add and remove tracks, and you can add, remove, and scale time ranges.】
+    AVMutableComposition *mixComposition = [[AVMutableComposition alloc] init];
+    //3 视频通道  工程文件中的轨道，有音频轨、视频轨等，里面可以插入各种对应的素材
+    AVMutableCompositionTrack *videoTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo
+                                                                        preferredTrackID:kCMPersistentTrackID_Invalid];
+    
+    //把视频轨道数据加入到可变轨道中 这部分可以做视频裁剪TimeRange
+    [videoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, self.videoAsset.duration)
+                        ofTrack:[[self.videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0]
+                         atTime:kCMTimeZero error:nil];
+    
+    
+    //音频通道
+    //声音采集
+    AVURLAsset * audioAsset = [[AVURLAsset alloc] initWithURL:videoUrl options:nil];
+    AVMutableCompositionTrack * audioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
+    //音频采集通道
+    AVAssetTrack * audioAssetTrack = [[audioAsset tracksWithMediaType:AVMediaTypeAudio] firstObject];
+    [audioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, self.videoAsset.duration) ofTrack:audioAssetTrack atTime:kCMTimeZero error:nil];
+    
+    
+    //3.1 AVMutableVideoCompositionInstruction 视频轨道中的一个视频，可以缩放、旋转等
+    AVMutableVideoCompositionInstruction *mainInstruction = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
+    mainInstruction.timeRange = CMTimeRangeMake(kCMTimeZero, self.videoAsset.duration);
+    
+    
+    
+    // 3.2 AVMutableVideoCompositionLayerInstruction 一个视频轨道，包含了这个轨道上的所有视频素材
+    AVMutableVideoCompositionLayerInstruction *videolayerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoTrack];
+    AVAssetTrack *videoAssetTrack = [[self.videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
+    UIImageOrientation videoAssetOrientation_  = UIImageOrientationUp;
+    BOOL isVideoAssetPortrait_  = NO;
+    CGAffineTransform videoTransform = videoAssetTrack.preferredTransform;
+    if (videoTransform.a == 0 && videoTransform.b == 1.0 && videoTransform.c == -1.0 && videoTransform.d == 0) {
+        videoAssetOrientation_ = UIImageOrientationRight;
+        isVideoAssetPortrait_ = YES;
+    }
+    if (videoTransform.a == 0 && videoTransform.b == -1.0 && videoTransform.c == 1.0 && videoTransform.d == 0) {
+        videoAssetOrientation_ =  UIImageOrientationLeft;
+        isVideoAssetPortrait_ = YES;
+    }
+    if (videoTransform.a == 1.0 && videoTransform.b == 0 && videoTransform.c == 0 && videoTransform.d == 1.0) {
+        videoAssetOrientation_ =  UIImageOrientationUp;
+    }
+    if (videoTransform.a == -1.0 && videoTransform.b == 0 && videoTransform.c == 0 && videoTransform.d == -1.0) {
+        videoAssetOrientation_ = UIImageOrientationDown;
+    }
+    [videolayerInstruction setTransform:videoAssetTrack.preferredTransform atTime:kCMTimeZero];
+    [videolayerInstruction setOpacity:0.0 atTime:self.videoAsset.duration];
+    
+    // 3.3 - Add instructions
+    mainInstruction.layerInstructions = [NSArray arrayWithObjects:videolayerInstruction,nil];
+    //AVMutableVideoComposition：管理所有视频轨道，可以决定最终视频的尺寸，裁剪需要在这里进行
+    AVMutableVideoComposition *mainCompositionInst = [AVMutableVideoComposition videoComposition];
+    
+    CGSize naturalSize;
+    if(isVideoAssetPortrait_){
+        naturalSize = CGSizeMake(videoAssetTrack.naturalSize.height, videoAssetTrack.naturalSize.width);
+    } else {
+        naturalSize = videoAssetTrack.naturalSize;
+    }
+    
+    float renderWidth, renderHeight;
+    renderWidth = naturalSize.width;
+    renderHeight = naturalSize.height;
+    mainCompositionInst.renderSize = CGSizeMake(renderWidth, renderHeight);
+    mainCompositionInst.instructions = [NSArray arrayWithObject:mainInstruction];
+    mainCompositionInst.frameDuration = CMTimeMake(1, 30);
+//    [self applyVideoEffectsToComposition:mainCompositionInst size:naturalSize];
+    [self applyVideoEffectsToComposition:mainCompositionInst WithWaterImg:[UIImage imageNamed:@""] WithCoverImage:[UIImage imageNamed:@"ball03"] WithQustion:@"点点找人" size:naturalSize];
+    // 4 - 输出路径
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *myPathDocs =  [documentsDirectory stringByAppendingPathComponent:
+                             [NSString stringWithFormat:@"FinalVideo-%d.mov",arc4random() % 1000]];
+    videoUrl = [NSURL fileURLWithPath:myPathDocs];
+    
+    // 5 - 视频文件输出
+    AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:mixComposition
+                                                                      presetName:AVAssetExportPresetHighestQuality];
+    exporter.outputURL= videoUrl;
+    exporter.outputFileType = AVFileTypeQuickTimeMovie;
+    exporter.shouldOptimizeForNetworkUse = YES;
+    exporter.videoComposition = mainCompositionInst;
+    [exporter exportAsynchronouslyWithCompletionHandler:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            //这里是输出视频之后的操作，做你想做的
+//            [self exportDidFinish:exporter];
+            PZXPlayerViewController *playVC = [[PZXPlayerViewController alloc] init];
+            playVC.videoUrl =  videoUrl;
+            
+            [self.navigationController pushViewController:playVC animated:YES];
+        });
+    }];
+    
+
+    
+    
+}
+
+
+/*  @param waterImg 水印图
+ *  @param CoverImage 覆盖图
+ *  @param question 文字水印
+ */
+- (void)applyVideoEffectsToComposition:(AVMutableVideoComposition *)composition WithWaterImg:(UIImage*)img WithCoverImage:(UIImage*)coverImg WithQustion:(NSString*)question  size:(CGSize)size {
+    
+    UIFont *font = [UIFont systemFontOfSize:30.0];
+    CATextLayer *subtitle1Text = [[CATextLayer alloc] init];
+    [subtitle1Text setFontSize:30];
+    [subtitle1Text setString:question];
+    [subtitle1Text setAlignmentMode:kCAAlignmentCenter];
+    [subtitle1Text setForegroundColor:[[UIColor whiteColor] CGColor]];
+    subtitle1Text.masksToBounds = YES;
+    subtitle1Text.cornerRadius = 23.0f;
+    [subtitle1Text setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.5].CGColor];
+    CGSize textSize = [question sizeWithAttributes:[NSDictionary dictionaryWithObjectsAndKeys:font,NSFontAttributeName, nil]];
+    [subtitle1Text setFrame:CGRectMake(50, 100, textSize.width+20, textSize.height+10)];
+    
+    //水印
+    CALayer *imgLayer = [CALayer layer];
+    imgLayer.contents = (id)img.CGImage;
+    //    imgLayer.bounds = CGRectMake(0, 0, size.width, size.height);
+    imgLayer.bounds = CGRectMake(0, 0, 210, 50);
+    imgLayer.position = CGPointMake(size.width/2.0, size.height/2.0);
+    
+    //第二个水印
+    CALayer *coverImgLayer = [CALayer layer];
+    coverImgLayer.contents = (id)coverImg.CGImage;
+    //    [coverImgLayer setContentsGravity:@"resizeAspect"];
+    coverImgLayer.bounds =  CGRectMake(50, 200,210, 50);
+    coverImgLayer.position = CGPointMake(size.width/4.0, size.height/4.0);
+    
+    // 2 - The usual overlay
+    CALayer *overlayLayer = [CALayer layer];
+    [overlayLayer addSublayer:subtitle1Text];
+    [overlayLayer addSublayer:imgLayer];
+    overlayLayer.frame = CGRectMake(0, 0, size.width, size.height);
+    [overlayLayer setMasksToBounds:YES];
+    
+    CALayer *parentLayer = [CALayer layer];
+    CALayer *videoLayer = [CALayer layer];
+    parentLayer.frame = CGRectMake(0, 0, size.width, size.height);
+    videoLayer.frame = CGRectMake(0, 0, size.width, size.height);
+    [parentLayer addSublayer:videoLayer];
+    [parentLayer addSublayer:overlayLayer];
+    [parentLayer addSublayer:coverImgLayer];
+    
+    //设置封面
+    CABasicAnimation *anima = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    anima.fromValue = [NSNumber numberWithFloat:1.0f];
+    anima.toValue = [NSNumber numberWithFloat:0.0f];
+    anima.repeatCount = 0;
+    anima.duration = 5.0f;  //5s之后消失
+    [anima setRemovedOnCompletion:NO];
+    [anima setFillMode:kCAFillModeForwards];
+    anima.beginTime = AVCoreAnimationBeginTimeAtZero;
+    [coverImgLayer addAnimation:anima forKey:@"opacityAniamtion"];
+    
+    composition.animationTool = [AVVideoCompositionCoreAnimationTool
+                                 videoCompositionCoreAnimationToolWithPostProcessingAsVideoLayer:videoLayer inLayer:parentLayer];
+    
 }
 @end
